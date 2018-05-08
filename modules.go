8701890a69f2ec49b668b02c3f335287ce08aed3
SandsNic <nicole.sands@ipreo.com>
2018-05-08 19:49:24 -0400

    logstash

--- modules.go
+++ modules.go
@@ -5,5 +5,6 @@ import (
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/tls"
 	_ "github.com/gliderlabs/logspout/transports/udp"
+	_ "github.com/looplab/logspout-logstash"
 	_ "github.com/remind101/logspout-kinesis"
 )
