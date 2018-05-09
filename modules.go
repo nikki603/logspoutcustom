4b0f76098c75a500424373c0ea9150304023da4a
SandsNic <nicole.sands@ipreo.com>
2018-05-08 20:19:33 -0400

    modules

--- modules.go
+++ modules.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	_ "github.com/looplab/logspout-logstash"
 	_ "github.com/gliderlabs/logspout/httpstream"
     _ "github.com/gliderlabs/logspout/routesapi"
 	_ "github.com/remind101/logspout-kinesis"
-	_ "github.com/looplab/logspout-logstash"
 )
